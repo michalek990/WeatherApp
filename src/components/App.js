9b47071a18320fb131f437b3c3cd7e914767a702
michalek990 <93988101+michalek990@users.noreply.github.com>
2023-03-05 17:07:22 +0100

    function fetch

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,20 @@ import Form from './Form';
 import Result from './Result';
 import '../components/App';
 
+const APIKey = '6f0884425a8af17d336368ae24870812';
 
 class App extends Component{
 
   state = {
-    value: ""
+    value: "",
+    date: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    temp: "",
+    preasure: "",
+    wind: "",
+    err: false 
   }
 
   handleInputChange = (e) =>{
@@ -15,11 +24,52 @@ class App extends Component{
       value: e.target.value
     })
   }
+
+  handleCitySubmit = (e) =>{
+    e.preventDefault()
+    console.log("dziala");
+    const API =  `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=6f0884425a8af17d336368ae24870812&units=metric`;
+
+    fetch(API)
+    .then(response => {
+      if(response.ok){
+        return response
+      }
+      throw Error("Failed")
+    })
+    .then(response => response.json())
+    .then(data => {
+      const time = new Date().toLocaleDateString()
+      this.setState({
+        err: false,
+        date: time,
+        city: this.state.value,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        temp: data.main.temp,
+        preasure: data.main.pressure,
+        wind: data.wind.speed,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      this.setState(state => ({
+        err: true,
+        city: this.state.value
+      }))
+    })
+  }
+  
   render(){
     return (
       <div className='App'>
-        <Form value={this.state.value} change = {this.handleInputChange}/>
-        <Result />
+        <Form value={this.state.value} 
+        change = {this.handleInputChange}
+        submit = {this.handleCitySubmit}
+        />
+        <Result 
+        weather = {this.state}
+        />
       </div>
     );
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const Result = () => {
+const Result = props => {
+    const {err, city, date, sunrise, sunset, preasure, wind} = props.weather
+
     return ( 
-        <div>
-            Rezultat
-        </div>
+        <React.Fragment>
+            <div>Pogoda dla: {city} </div>
+            <div>Pogoda z dnia: {date}</div> 
+            <div>Wschód słonca: {sunrise} </div>
+            <div>Zachód słonca: {sunset}</div> 
+            <div>Ciśnienie: {preasure} </div>
+            <div>Wiatr: {wind}</div>
+        </React.Fragment> 
      );
 }
  
